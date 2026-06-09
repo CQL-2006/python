f556d8774842545d74109742cd18e25fbacca491
CQL-2006 <2254771860@qq.com>
2026-06-09 13:46:17 +0800

    绘制散点图

--- 绘图教程.py
+++ 绘图教程.py
@@ -42,40 +42,49 @@ def grad_f(x):
 # plt.legend()
 # plt.show()
 
-def record_footprints(strat_point,step_size,steps = 30):
-    x = np.array(strat_point,dtype = float)
+# def record_footprints(strat_point,step_size,steps = 30):
+#     x = np.array(strat_point,dtype = float)
     
-    foot_x = []
-    foot_y = []
-    for i in range(steps):
-        foot_x.append(x[0])
-        foot_y.append(x[1])
+#     foot_x = []
+#     foot_y = []
+#     for i in range(steps):
+#         foot_x.append(x[0])
+#         foot_y.append(x[1])
         
-        x = x - step_size*grad_f(x)
-    return foot_x,foot_y 
+#         x = x - step_size*grad_f(x)
+#     return foot_x,foot_y 
 
-fx,fy = record_footprints([5.0,5.0],0.1,steps = 20)
+# fx,fy = record_footprints([5.0,5.0],0.1,steps = 20)
 
-#生成等高线
-x_grid = np.linspace(-6,6,100)
-y_grid = np.linspace(-6,6,100)
+# #生成等高线
+# x_grid = np.linspace(-6,6,100)
+# y_grid = np.linspace(-6,6,100)
 
-X,Y = np.meshgrid(x_grid,y_grid)
+# X,Y = np.meshgrid(x_grid,y_grid)
 
-Z = X**2 + 2 * Y**2 
-#画线
-plt.contour(X,Y,Z,levels = 20,cmap = 'viridis',alpha = 0.6)
-plt.plot(fx,fy,'r-o',label = 'Alpgorithm Path')
+# Z = X**2 + 2 * Y**2 
+# #画线
+# plt.contour(X,Y,Z,levels = 20,cmap = 'viridis',alpha = 0.6)
+# plt.plot(fx,fy,'r-o',label = 'Alpgorithm Path')
 
-#标起点和终点
-plt.plot(fx[0],fy[0],'go',markersize = 10,label = 'Start')
-plt.plot(fx[-1],fy[-1],'ys',markersize = 10,label = 'End')
+# #标起点和终点
+# plt.plot(fx[0],fy[0],'go',markersize = 10,label = 'Start')
+# plt.plot(fx[-1],fy[-1],'ys',markersize = 10,label = 'End')
 
-#收尾
-plt.title('Footprint on the Mountain')
-plt.xlabel("x")
-plt.ylabel("y")
-plt.legend()
-plt.axis('equal')#让x,y比例一致
-plt.show()
+# #收尾
+# plt.title('Footprint on the Mountain')
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.legend()
+# plt.axis('equal')#让x,y比例一致
+# plt.show()
 
+x_eg1 = [1,2,3,4,5]
+y_eg1 = [2,5,3,8,4]
+
+sizes = [10,50,100,300,400]
+
+colors = [1,2,3,4,5]
+plt.scatter(x_eg1,y_eg1,s = sizes ,c=colors,cmap = 'coolwarm',alpha = 0.7)
+plt.title("Scatter Plot:Different Sizes and Colors")
+plt.show()  
